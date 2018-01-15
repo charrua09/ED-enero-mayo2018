c08fe777e52e2727d4c3a077842a871da09d630a
SGUARDATB <SGUARDATB@113DAC26A.itam.mx>
2018-01-15 17:19:24 -0600

    Definición de la clase que representa a un Circulo

---
+++ EDenero-mayo2018/src/edenero/mayo2018/Circulo.java
@@ -0,0 +1,32 @@
+package edenero.mayo2018;
+
+
+
+/**
+ *
+ * @author SGUARDATB
+ */
+public class Circulo {
+    private double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" + "radio=" + radio + '}';
+    }
+    
+}
